472f7342373687382036c421b1ae85116b3000f0
NickBurness <Nickburness93@outlook.com>
2020-09-24 19:19:35 +0100

    add content and mass style changes.
    TODO: tidy sass style sheet, file is growing and is currently out of order.

--- site.js
+++ site.js
@@ -4,6 +4,7 @@ let toggleIcon = document.getElementById("toggleIcon");
 let lightswitch = document.getElementById("lightswitch");
 let body = document.querySelector("body");
 let header = document.querySelector("header");
+let scrollButton = document.getElementById("scrollButton");
 
 function toggleNav() {
     nav.classList.toggle("hidden");
@@ -25,4 +26,21 @@ function toggleLight() {
 
     let headerClass = header.className;
     header.className = headerClass === "lm" ? "dm" : "lm";
+}
+
+window.onscroll = function () { DetectScroll() };
+
+function DetectScroll() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollButton.style.display = "block";
+    }
+    else {
+        scrollButton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTopOfPage() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
